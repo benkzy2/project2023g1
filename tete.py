292d19a2555cb30ba80c36aacc5f9b03f9fd096f
zlrfn <91626989+zlrfn@users.noreply.github.com>
2023-02-06 15:50:51 +0800

    test
    Test

---
+++ tete.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'<html><body><h1>Hello, World!</h1></body></html>')
+
+server = HTTPServer(('', 8000), RequestHandler)
+print('Serving on http://localhost:8000/')
+server.serve_forever()
